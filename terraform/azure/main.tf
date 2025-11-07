8b91e101cee30e6b44779cde0b70092a55fd4500
redmond <rafosh77@gmail.com>
2025-11-07 00:48:28 -0800

    terraform/azure/main.tf

--- terraform/azure/main.tf
+++ terraform/azure/main.tf
@@ -1,10 +1,17 @@
 terraform {
   required_version = ">= 1.6"
   required_providers {
-    azurerm = { source = "hashicorp/azurerm", version = "~> 3.118" }
-    random  = { source = "hashicorp/random",  version = "~> 3.6" }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.7"
+    }
   }
 }
+
 
 provider "azurerm" {
   features {}
